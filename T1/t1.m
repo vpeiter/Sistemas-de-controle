1aab849d933915adf3dbbc2224574b3c9b05f1b3
VPeiter <viniciuspeiter@gmail.com>
2018-08-26 22:56:25 -0300

    tarefa 1 questao 3

--- T1/t1.m
+++ T1/t1.m
@@ -28,3 +28,19 @@ title('Curva estatica de Cb');
 xlabel('U');
 ylabel('Caf');
 zlabel('Cb');
+
+%% Questao 3
+% ponto de operaçao
+u_0 = 1;
+Caf_0 = 5.1;
+Ca_0 = 0.7192;
+Cb_0 = 2.345;
+% TFs
+s = tf('s');
+Ca_U = (Caf_0 - Ca_0)/(s + k1 + 2*Ca_0*k3 + u_0); % Ca/U
+Ca_Caf = (u_0)/(s + k1 + 2*Ca_0*k3 + u_0); % Ca/Caf
+Cb_Ca = (k1)/(s + k2 + u_0); % Cb/Ca
+Cb_U = (Cb_0)/(s + k2 + u_0); % Cb/U
+
+
+
